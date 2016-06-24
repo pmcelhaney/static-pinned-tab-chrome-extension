98f61823779558c327a9b68d9cae448eb090a69b
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2016-06-24 15:41:34 -0400

    Binds to onUpdated, a bit more consistent behavior.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.tabs.onActivated.addListener(function(tab) {
-  chrome.tabs.get(tab.tabId, function(t) {
-    if (t.pinned) {
+chrome.tabs.onUpdated.addListener(function(tabId) {
+  chrome.tabs.get(tabId, function(tab) {
+    if (tab.pinned) {
       chrome.tabs.executeScript({
         code: "document.querySelectorAll('a[href]').forEach(a => a.setAttribute('target', '_blank'));"
       });
