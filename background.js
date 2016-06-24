520a408f82c2692a6572293ae8b3ef83c90e6f11
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2016-06-24 14:07:39 -0400

    first commit

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.tabs.onActivated.addListener(function(tab) {
+  chrome.tabs.get(tab.tabId, function (t) {
+    if(t.pinned) {
+      chrome.tabs.executeScript({
+        code: "document.querySelectorAll('a[href]').forEach(a => a.setAttribute('target', '_blank'));"
+      });
+    }
+  });
+});
